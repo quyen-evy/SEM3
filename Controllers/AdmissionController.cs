ffa721ffd2d988fcd091464b8edc99a4e07e6d2e
MinhVA <anhminh25041997@yahoo.com>
2020-06-20 14:46:07 +0700

    Save 2h44

--- Controllers/AdmissionController.cs
+++ Controllers/AdmissionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using projectsem3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace projectsem3.Controllers
 {
     public class AdmissionController : Controller
     {
+        private ManageStudentEntities ManageStudent = new ManageStudentEntities();
         // GET: Admission
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Ads()
+        {
+            return Content("fail vcl");
         }
 
     }
