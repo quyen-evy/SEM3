fe7a7a535d1dffb4312acb8965fc4a4aeaecb825
MinhVA <anhminh25041997@yahoo.com>
2020-06-23 19:41:36 +0700

    save

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -11,15 +11,16 @@ namespace projectsem3.Controllers
     {
         private ManageStudentEntities ManageStudent = new ManageStudentEntities();
         // GET: Course
-        public ActionResult Index()
+        public ActionResult Index(int id = 1)
         {
-            List<COURSE> course = ManageStudent.COURSEs.Where(m => m.Status == false).ToList<COURSE>();
+            List<COURSE> courses = ManageStudent.COURSEs.Where(v => v.Status == false && v.Id == id).ToList<COURSE>();
+            List<COURSE> course = ManageStudent.COURSEs.Where(v => v.Status == false).ToList<COURSE>();
             TempData["courses"] = course;
             List<FACILITy> facilities = ManageStudent.FACILITIES.Where(v => v.Status == false).ToList<FACILITy>();
             TempData["facilities"] = facilities;
             List<DEPARTMENT> department = ManageStudent.DEPARTMENTs.Where(u => u.Status == false).ToList<DEPARTMENT>();
             TempData["department"] = department;
-            return View(course);
+            return View(courses);
         }
     }
 }
